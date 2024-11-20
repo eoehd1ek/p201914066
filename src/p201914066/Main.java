a0442ec3a575548fc6d0966cf5eee395ef513c0a
eoehd1ek <eoehd2ek@gmail.com>
2024-11-20 09:45:16 +0900

    two

--- src/p201914066/Main.java
+++ src/p201914066/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("201914066 김대동");
-    }
+	public static void main(String[] args) {
+		System.out.println("201914066 김대동");
+		System.out.println(Hello.sum(10));
+		System.out.println(Hello.sum(100));
+	}
 }
