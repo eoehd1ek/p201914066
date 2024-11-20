952036746ef724269463aa234fa66c2fd4aaf855
eoehd1ek <eoehd2ek@gmail.com>
2024-11-20 09:48:40 +0900

    three

--- src/p201914066/Main.java
+++ src/p201914066/Main.java
@@ -2,6 +2,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("201914066 김대동");
 		System.out.println(Hello.sum(10));
-		System.out.println(Hello.sum(100));
+        System.out.println(Hello.sum(100));
+        System.out.println(Hello.sum(1000));
 	}
 }
