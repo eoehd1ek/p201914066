b79acf1b10a281109d0875a2f9331c02b20a72b8
eoehd1ek <eoehd2ek@gmail.com>
2024-11-20 09:54:47 +0900

    six

--- src/p201914066/Main.java
+++ src/p201914066/Main.java
@@ -1,10 +1,13 @@
+import static java.lang.System.out;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("201914066 김대동");
-		System.out.println(Hello.sum(10));
-		System.out.println(Hello.sum(100));
-		System.out.println(Hello.sum(1000));
-		System.out.println(Sansu.max(new int[] { 8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8 }));
-		System.out.println(Sansu.max(new int[] { -2, 5, 6, 9, -6, 1, 7, 2, 8, 10 }));
-	}
+    public static void main(String[] args) {
+        // 자신의 학번과 이름을 입력하라
+        out.println("201514099 홍길동");
+        out.println(Hello.sum(10));
+        out.println(Hello.sum(100));
+        out.println(Hello.sum(1000));
+        out.println(Sansu.max(new int[] { 8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8 }));
+        out.println(Sansu.max(new int[] { -2, 5, 6, 9, -6, 1, 7, 2, 8, 10 }));
+    }
 }
